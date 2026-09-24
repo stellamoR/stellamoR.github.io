0aef4e29ed572e50158794f41876d133b8f1199e
stellamoR <sternbergrke@gmail.com>
2026-09-24 18:51:50 +0200

    default site to light mode

--- _plugins/site_custom_styles.rb
+++ _plugins/site_custom_styles.rb
@@ -6,7 +6,14 @@ inject_system_font = lambda do |document|
   baseurl = document.site.config["baseurl"].to_s.chomp("/")
   cache_key = document.site.time.to_i
   stylesheet = %(<link rel="stylesheet" href="#{baseurl}/assets/css/site-custom.css?v=#{cache_key}">)
-  document.output = document.output.sub("</head>", "  #{stylesheet}\n</head>")
+  light_theme_default = <<~HTML.strip
+    <script>
+      if (!["light", "dark", "system"].includes(localStorage.getItem("theme"))) {
+        localStorage.setItem("theme", "light");
+      }
+    </script>
+  HTML
+  document.output = document.output.sub("</head>", "  #{stylesheet}\n  #{light_theme_default}\n</head>")
 end
 
 Jekyll::Hooks.register :pages, :post_render, &inject_system_font
