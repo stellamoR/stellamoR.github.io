a1623a4721be39c743602a6f3b9133207cbc876a
stellamoR <sternbergrke@gmail.com>
2026-09-24 16:19:20 +0200

    tighten CV spacing

--- _plugins/cv_custom_styles.rb
+++ _plugins/cv_custom_styles.rb
@@ -4,6 +4,7 @@ Jekyll::Hooks.register :pages, :post_render do |page|
   next unless page.data["layout"] == "cv"
 
   baseurl = page.site.config["baseurl"].to_s.chomp("/")
-  stylesheet = %(<link rel="stylesheet" href="#{baseurl}/assets/css/cv-custom.css">)
+  cache_key = page.site.time.to_i
+  stylesheet = %(<link rel="stylesheet" href="#{baseurl}/assets/css/cv-custom.css?v=#{cache_key}">)
   page.output = page.output.sub("</head>", "  #{stylesheet}\n</head>")
 end
